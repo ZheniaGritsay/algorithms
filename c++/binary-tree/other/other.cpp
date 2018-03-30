e53ebc0d4746dc449daab879b8d632f982bb94f0
Yevhen Hrytsai <yevhen_hrytsai@epam.com>
2018-03-30 16:07:10 +0300

    resolve: 	- deserialize 	- serialize

--- c++/binary-tree/other/other.cpp
+++ c++/binary-tree/other/other.cpp
@@ -3,6 +3,7 @@
 #include "../construct/tree_construct.h"
 #include <queue>
 #include <sstream>
+#include <iostream>
 
 void populate_next_right_pointer(TreeLinkNode *root)
 {
@@ -33,22 +34,6 @@ void populate_next_right_pointer(TreeLinkNode *root)
     }
 }
 
-std::vector<int> convert_to_int(std::vector<std::string> vec)
-{
-
-    std::vector<int> converted;
-    int val;
-    for (std::string& s : vec)
-    {
-        std::stringstream ss(s);
-
-        ss >> val;
-        converted.push_back(val);
-    }
-
-    return converted;
-}
-
 std::vector<std::string> split(std::string line, char delimiter)
 {
     std::vector<std::string> splitted;
@@ -63,31 +48,96 @@ std::vector<std::string> split(std::string line, char delimiter)
     return splitted;
 }
 
-TreeNode *lvl_order(int arr[], TreeNode *root, int i, int n)
+TreeNode *lvl_order_deser(std::vector<std::string> vals, TreeNode *root, int i, int n)
 {
     if (i < n)
     {
-        TreeNode *tmp = new TreeNode(arr[i]);
+        std::string s = vals[i];
+        int v = std::stoi(s);
+
+        TreeNode *tmp = new TreeNode(v);
         root = tmp;
 
-        root->left = lvl_order(arr, root->left, i * 2 + 1, n);
-        root->right = lvl_order(arr, root->right, i * 2 + 2, n);
+        TreeNode* left_node = nullptr;
+        TreeNode* right_node = nullptr;
+        int ileft = i * 2 + 1;
+        int iright = ileft + 1;
+
+        if (ileft < n && vals[ileft] != "null")
+        {
+            left_node = lvl_order_deser(vals, root->left, ileft, n);
+        }
+        if (iright < n && vals[iright] != "null")
+        {
+            right_node = lvl_order_deser(vals, root->right, iright, n);
+        }
+
+        root->left = left_node;
+        root->right = right_node;
     }
 
     return root;
 }
 
+std::string lvl_order_ser(TreeNode *root)
+{
+    std::vector<std::string> node_vals;
+    std::queue<TreeNode *> node_queue;
+    node_queue.push(root);
+
+    while (1)
+    {
+        size_t node_count = node_queue.size();
+        if (node_count == 0)
+            break;
+
+        for (;node_count > 0; --node_count)
+        {
+            TreeNode *curr = node_queue.front();
+            node_queue.pop();
+
+            if (curr)
+                node_vals.push_back(std::to_string(curr->val));
+            else
+            {
+                node_vals.push_back("null");
+                continue;
+            }
+
+            node_queue.push(curr->left);
+            node_queue.push(curr->right);
+        }
+    }
+
+    std::string serialized = "[";
+
+    for (auto it = node_vals.begin(); it != (node_vals.end() - 4); it++)
+    {
+        serialized += (*it);
+        if (it != (node_vals.end() - 5))
+            serialized += ",";
+    }
+
+    serialized += "]";
+
+    return serialized;
+}
+
 std::string serialize(TreeNode *root)
 {
+    if (root == nullptr)
+        return "[]";
 
+    return lvl_order_ser(root);
 }
 
 TreeNode *deserialize(std::string data)
 {
-    std::vector<std::string> splitted = split(data, ',');
-    std::vector<int> nums = convert_to_int(splitted);
+    if (data.size() == 0)
+        return nullptr;
 
-    TreeNode *root = lvl_order(&nums[0], root, 0, nums.size());
+    std::vector<std::string> splitted = split(data.substr(1, data.length() - 2), ',');
+    TreeNode *root = lvl_order_deser(splitted, root, 0, splitted.size());
 
     return root;
 }
--- c++/main.cpp
+++ c++/main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
 #include "binary-tree/other/other.h"
-#include "binary-tree/construct/tree_construct.h"
-#include "binary-tree/binary_tree.h"
-#include "binary-tree/other/other.h"
 
 int main() {
 
-//    int tree[] = {1, 2, 3, 4, 5, 6, 7};
-//    size_t  length = sizeof(tree) / sizeof(tree[0]);
-//
-//    TreeLinkNode* root = construct_level_order(tree, root, 0, length);
-//    root->next = nullptr;
-//
-//    populate_next_right_pointer(root);
+    std::string str = "[1,2,3,null,null,4,5]";
 
-    std::string str = "1,2,3,5,7,4,5";
+    TreeNode *root = deserialize(str);
 
-    deserialize(str);
+    std::string ser = serialize(root);
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
